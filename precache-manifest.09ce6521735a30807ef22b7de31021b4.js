35fcd5bcf32f0ed6d2b558fe207a0ebc4b674397
ItsFenil <102537116+ItsFenil@users.noreply.github.com>
2023-02-07 16:16:57 +0530

    Updates

---
+++ precache-manifest.09ce6521735a30807ef22b7de31021b4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd4f3529e7010c2bcba16c96e2b6a76",
+    "url": "/Aadi-Solution/index.html"
+  },
+  {
+    "revision": "0d43c9effdf66a5d8805",
+    "url": "/Aadi-Solution/static/css/main.04df53d7.chunk.css"
+  },
+  {
+    "revision": "a6fe8bb052f48af5e477",
+    "url": "/Aadi-Solution/static/js/2.81e7c98c.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/Aadi-Solution/static/js/2.81e7c98c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d43c9effdf66a5d8805",
+    "url": "/Aadi-Solution/static/js/main.44292c20.chunk.js"
+  },
+  {
+    "revision": "30514dd7a679b1d7a575",
+    "url": "/Aadi-Solution/static/js/runtime-main.d1e96e79.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/Aadi-Solution/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/Aadi-Solution/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/Aadi-Solution/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/Aadi-Solution/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/Aadi-Solution/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/Aadi-Solution/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/Aadi-Solution/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/Aadi-Solution/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/Aadi-Solution/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/Aadi-Solution/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/Aadi-Solution/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/Aadi-Solution/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/Aadi-Solution/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/Aadi-Solution/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
